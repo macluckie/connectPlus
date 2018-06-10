059fec1dfe0469af8dc16cb61237b24c186ccad2
macluckie <dimitri.macluckie@gmail.com>
2018-06-10 22:45:31 +0200

    paramétragewisywyg

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,8 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new FOS\UserBundle\FOSUserBundle(),
+            new Ivory\CKEditorBundle\IvoryCKEditorBundle(),
+
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,6 +5,11 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
 
 class DefaultController extends Controller
 {
@@ -13,9 +18,27 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
+
+
+
+      $defaultData = array('message' => 'Type your message here');
+  $form = $this->createFormBuilder($defaultData)
+
+      ->add('message', TextareaType::class)
+      ->add('content', 'ckeditor')
+      ->add('send', SubmitType::class)
+      ->getForm();
+
+  $form->handleRequest($request);
+
+  if ($form->isSubmitted() && $form->isValid()) {
+      // data is an array with "name", "email", and "message" keys
+      $data = $form->getData();
+  }
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+            'form' => $form->createview(),
         ]);
     }
 }
