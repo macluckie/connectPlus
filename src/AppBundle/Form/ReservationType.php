fe6e955385582cc6907a5c1fcd9b5f8f89769962
macluckie <dimitri.macluckie@gmail.com>
2018-08-23 23:03:12 +0200

    amélioration du formulaire

--- src/AppBundle/Form/ReservationType.php
+++ src/AppBundle/Form/ReservationType.php
@@ -4,8 +4,21 @@ namespace AppBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Form;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
+
+
+
+
+
+
+
+
 
 class ReservationType extends AbstractType
 {
@@ -19,17 +32,53 @@ class ReservationType extends AbstractType
             ->add('lastname')
             ->add('mail')
             ->add('nombrepersonne')
-            ->add('Game')
-            ->add('console')
             ->add('salle')
-            ->add('reserver', SubmitType::class);
+            ->add('reserver', SubmitType::class)
+            ->add('game',EntityType::class,[
+                "class"=>"AppBundle:Game",])
+            ;
+
+
+
+
+
+                    $builder->get('game')->addEventListener(FormEvents::PRE_SUBMIT, function (FormEvent $event) {
+
+
+                        $form = $event->getForm();
+
+                        $data = $event->getData();
+
+                        var_dump($data->getGame());
+
+
+
+
+
+
+                        var_dump($data->getGame());
+
+                        $form->getParent()->add('console',EntityType::class,[
+                            'class'=>"AppBundle:Console",
+                            'choices'=>$data
+
+
+
+
+                        ]);
+                    });
+
+
+
+
     }/**
      * {@inheritdoc}
      */
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults(array(
-            'data_class' => 'AppBundle\Entity\Reservation'
+            'data_class' => 'AppBundle\Entity\Reservation',
+
 
         ));
     }
