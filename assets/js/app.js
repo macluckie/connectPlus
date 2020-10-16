c473755b2040b0bf024124d7abb902be73ad912a
dimitri.macluckie@gmail.com <dimitri.macluckie@gmail.com>
2020-10-16 09:28:48 +0200

    fix: change front quality

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,37 +5,37 @@ require('bootstrap');
 
 
 
-$(document).ready(function(){
-    $(".gameConsole ").click(function(){
-        let game =$(".gameConsole option:selected").text();
-        $.ajax({
-            url: 'http://localhost:8000/formajax',
-            method:'POST',
-            data: 'param='+game,
-            success: function(data){
-                        if ($(".newSelect").length == 1){
-                          $(".newSelect").remove();
-                        }
+$(document).ready(function () {
+  $(".gameConsole ").click(function () {
+    let game = $(".gameConsole option:selected").text();
+    $.ajax({
+      url: 'http://localhost:8000/formajax',
+      method: 'POST',
+      data: 'param=' + game,
+      success: function (data) {
+        if ($(".newSelect").length == 1) {
+          $(".newSelect").remove();
+        }
 
-                        $("#appbundle_reservation").append('<select id="appbundle_reservation_console" name="appbundle_reservation[console]" class="newSelect form-control"></select>')
-
-
-
-               for(i=0; i< data.length; i++){
-
-
-                    $(".newSelect").append('<option value="'+ data[i]['id'] +'" >'+ data[i]['console'] +' </option>');
-
-
-            }
-
-
-        }, dataType: 'json'
-
-
-        })
+        $("#appbundle_reservation").append('<select id="appbundle_reservation_console" name="appbundle_reservation[console]" class="newSelect form-control"></select>')
 
 
 
-})
+        for (i = 0; i < data.length; i++) {
+
+
+          $(".newSelect").append('<option value="' + data[i]['id'] + '" >' + data[i]['console'] + ' </option>');
+
+
+        }
+
+
+      }, dataType: 'json'
+
+
+    })
+
+
+
+  })
 })
